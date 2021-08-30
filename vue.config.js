6d33bc7711e3d457f71d1efaae3cae5e82f4e935
wuanlin <wuanlin@xinfud.com>
2021-08-30 09:36:18 +0800

    change output dir

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
+const path = require('path')
+const output = path.resolve(__dirname, 'docs')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  outputDir: output,
+  chainWebpack: config => {
+    config
+      .output
+      .filename('js/[name].js')
+      .chunkFilename('js/[name].js')
+    config
+      .plugin('html')
+      .tap(options => {
+        options[0].title = 'VueCountdownPlus Examples'
+        return options
+      })
+    config
+      .plugin('extract-css')
+      .tap(options => {
+        options[0].filename = options[0].chunkFilename = 'css/[name].css'
+        return options
+      })
+  }
 }
